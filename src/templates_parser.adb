6ae6cbf91e2fcedb8c7e62a54e3a3af3cce123e2
Pascal Obry <obry@adacore.com>
1999-11-01 11:10:30 +0000

    Implement IF, TABLE, INCLUDE

--- src/templates_parser.adb
+++ src/templates_parser.adb
@@ -1,12 +1,33 @@
 
--- $Id$
+--  $Id$
 
+with Ada.Exceptions;
 with Ada.Text_IO;
+with Ada.Strings.Fixed;
+with Ada.Strings.Maps;
 with Ada.Strings.Unbounded;
+with Strings_Cutter;
 
 package body Templates_Parser is
 
    use Ada;
+   use Ada.Strings;
+
+   subtype Buffer_Type is String (1 .. 1_024);
+
+   Table_Token     : constant String := "@@TABLE@@";
+   End_Table_Token : constant String := "@@END_TABLE@@";
+   If_Token        : constant String := "@@IF@@";
+   Else_Token      : constant String := "@@ELSE@@";
+   End_If_Token    : constant String := "@@END_IF@@";
+   Include_Token   : constant String := "@@INCLUDE@@";
+
+   subtype Table_Range     is Positive range Table_Token'Range;
+   subtype End_Table_Range is Positive range End_Table_Token'Range;
+   subtype If_Range        is Positive range If_Token'Range;
+   subtype Else_Range      is Positive range Else_Token'Range;
+   subtype End_If_Range    is Positive range End_If_Token'Range;
+   subtype Include_Range   is Positive range Include_Token'Range;
 
    -----------
    -- Assoc --
@@ -14,13 +35,32 @@ package body Templates_Parser is
 
    function Assoc (Variable  : in String;
                    Value     : in String;
-                   Begin_Tag : in String := Default_Begin_Tag;
-                   End_Tag   : in String := Default_End_Tag)
+                   Begin_Tag : in String    := Default_Begin_Tag;
+                   End_Tag   : in String    := Default_End_Tag;
+                   Separator : in Character := Default_Separator)
                    return Association is
    begin
       return Association'
-        (Strings_1024.To_Bounded_String (Begin_Tag & Variable & End_Tag),
-         Strings_1024.To_Bounded_String (Value));
+        (To_Unbounded_String (Begin_Tag & Variable & End_Tag),
+         To_Unbounded_String (Value),
+         Separator);
+   end Assoc;
+
+   -----------
+   -- Assoc --
+   -----------
+
+   function Assoc (Variable  : in String;
+                   Value     : in Boolean;
+                   Begin_Tag : in String    := Default_Begin_Tag;
+                   End_Tag   : in String    := Default_End_Tag)
+                   return Association is
+   begin
+      if Value then
+         return Assoc (Variable, "TRUE", Begin_Tag, End_Tag);
+      else
+         return Assoc (Variable, "FALSE", Begin_Tag, End_Tag);
+      end if;
    end Assoc;
 
    -----------
@@ -32,32 +72,452 @@ package body Templates_Parser is
                    return String
    is
 
-      procedure Translate (Str : in out Strings_1024.Bounded_String) is
+      File         : Text_IO.File_Type;
+      Current_Line : Natural := 0;
+      Table_Level  : Natural := 0;
+      If_Level     : Natural := 0;
+
+      -----------------
+      -- Fatal_Error --
+      -----------------
+
+      procedure Fatal_Error (Message : in String) is
+      begin
+         Exceptions.Raise_Exception
+           (Template_Error'Identity,
+            "In " & Template_Filename
+            & " at line" & Natural'Image (Current_Line) & ' ' & Message);
+      end Fatal_Error;
+
+      -----------
+      -- Exist --
+      -----------
+
+      --  check that Tag exist in the translation table and that its value is
+      --  set to Value.
+
+      function Exist (Translations : in Translate_Table;
+                      Tag          : in String;
+                      Value        : in String)
+        return Boolean is
+      begin
+         for K in Translations'Range loop
+            if Translations (K).Variable = Tag
+              and then Translations (K).Value = Value then
+               return True;
+            end if;
+         end loop;
+         return False;
+      end Exist;
+
+      ---------------
+      -- Translate --
+      ---------------
+
+      --  Translate all tags in Str.
+
+      procedure Translate (Str : in out Unbounded_String) is
          Pos : Natural;
       begin
          for K in Translations'Range loop
-            Pos := Strings_1024.Index
-              (Str,
-               Strings_1024.To_String (Translations (K).Variable));
 
-            if Pos /= 0 then
-               Strings_1024.Replace_Slice
+            loop
+               Pos := Index
+                 (Str,
+                  To_String (Translations (K).Variable));
+
+               exit when Pos = 0;
+
+               Replace_Slice
                  (Str,
                   Pos,
-                  Pos + Strings_1024.Length (Translations (K).Variable) - 1,
-                  Strings_1024.To_String (Translations (K).Value));
-            end if;
+                  Pos + Length (Translations (K).Variable) - 1,
+                  To_String (Translations (K).Value));
+            end loop;
+
          end loop;
       end Translate;
 
-      use Ada.Strings.Unbounded;
+      --  Translate all tags in Str with the Nth Tag's value. This procedure
+      --  is used to build the tables. Tags used in a table are a set of
+      --  values separated by a special character.
 
-      Buffer : String (1 .. 1_024);
-      Last   : Natural;
-      Str    : Strings_1024.Bounded_String;
-      Result : Unbounded_String;
+      procedure Translate (Str  : in out Unbounded_String;
+                           N    : in     Positive;
+                           Stop :    out Boolean)
+      is
+         use Strings_Cutter;
+         Pos : Natural;
+         CS  : Cutted_String;
+      begin
+         Stop := False;
+         for K in Translations'Range loop
 
-      File   : Text_IO.File_Type;
+            loop
+               Pos := Index
+                 (Str,
+                  To_String (Translations (K).Variable));
+
+               exit when Pos = 0;
+
+               Create (CS,
+                       To_String (Translations (K).Value),
+                       String'(1 => Translations (K).Separator));
+
+               --  we stop when we reach the maximum number of field
+               Stop := (N = Field_Count (CS) or else Field_Count (CS) = 0);
+
+               --  if there is no value for the tag, just replace it with an
+               --  emptry string (i.e. removing it from the template).
+
+               if Field_Count (CS) = 0 then
+                  Replace_Slice
+                    (Str,
+                     Pos,
+                     Pos + Length (Translations (K).Variable) - 1,
+                     "");
+               else
+                  Replace_Slice
+                    (Str,
+                     Pos,
+                     Pos + Length (Translations (K).Variable) - 1,
+                     Field (CS, N));
+               end if;
+
+               Destroy (CS);
+            end loop;
+
+         end loop;
+      end Translate;
+
+      --  variables used by Parse
+
+      Buffer        : Buffer_Type;
+      Last          : Natural;
+      Result        : Unbounded_String;
+      Trimed_Buffer : Buffer_Type;
+
+      -------------------
+      -- Get_Next_Line --
+      -------------------
+
+      procedure Get_Next_Line (Buffer, Trimed_Buffer : out String;
+                               Last                  : out Natural);
+      pragma Inline (Get_Next_Line);
+
+      procedure Get_Next_Line (Buffer, Trimed_Buffer : out String;
+                               Last                  : out Natural) is
+      begin
+         Text_IO.Get_Line (File, Buffer, Last);
+         Current_Line := Current_Line + 1;
+
+         Fixed.Move (Fixed.Trim (Buffer (1 .. Last), Strings.Left),
+                     Trimed_Buffer);
+      end Get_Next_Line;
+
+
+      -----------
+      -- Parse --
+      -----------
+
+      function Parse (Line : in String) return String is
+
+         Str : Unbounded_String;
+
+
+
+         function Parse_If (Condition       : in String  := "";
+                            Check_Condition : in Boolean := True)
+           return String;
+         --  Parse an if statement (from If_Token to End_If_Token). Condition
+         --  is the if conditional part. It will be checked only if
+         --  Check_Condition is True.  At the time of this call If_Token as
+         --  been read. When returning the file index is positioned on the
+         --  line just after the End_If_Token is ready.
+
+         function Parse_Table return String;
+         --  Parse a table statement (from Table_Token to End_Table_Token). At
+         --  the time of this call Table_Token as been read. When returning
+         --  the file index is positioned on the line just after the
+         --  End_Table_Token line.
+
+         function Parse_Include (Filename : in String) return String;
+         --  Parse an include statement (Include_Token).  At the time of this
+         --  call Include_Token as been read. When returning the file index is
+         --  positioned on the line just after the Include_Token line.
+
+         function Parse_Table return String is
+            Buffer        : Buffer_Type;
+            Trimed_Buffer : Buffer_Type;
+            Last          : Natural;
+            Lines         : Unbounded_String;
+            Str           : Unbounded_String;
+            Result        : Unbounded_String;
+         begin
+
+            Table_Level := Table_Level + 1;
+
+            loop
+               Get_Next_Line (Buffer, Trimed_Buffer, Last);
+
+               if Last = 0 then
+                  Lines := Lines & ASCII.LF;
+               else
+                  if Trimed_Buffer (If_Range) = If_Token then
+                     Lines := Lines & Parse_If
+                       (Fixed.Trim
+                        (Trimed_Buffer (If_Range'Last + 2 .. Last), Both));
+
+                  elsif Trimed_Buffer (Table_Range) = Table_Token then
+                     Lines := Lines & Parse_Table;
+
+                  elsif Trimed_Buffer (Include_Range) = Include_Token then
+                     Lines := Lines & Parse_Include
+                       (Fixed.Trim
+                        (Trimed_Buffer (Include_Range'Last + 2 .. Last),
+                         Both));
+
+                  elsif Trimed_Buffer (End_Table_Range) = End_Table_Token then
+                     declare
+                        K    : Positive := 1;
+                        Stop : Boolean;
+                     begin
+                        loop
+                           Str := Lines;
+                           Translate (Str, K, Stop);
+
+                           Result := Result & To_String (Str);
+
+                           exit when Stop;
+
+                           K := K + 1;
+                        end loop;
+
+                        exit;
+                     end;
+
+                  elsif Trimed_Buffer (End_If_Range) = End_If_Token then
+                     Fatal_Error (End_If_Token & " found, "
+                                  & End_Table_Token & " expected.");
+
+                  else
+                     Lines := Lines & Buffer (1 .. Last) & ASCII.LF;
+                  end if;
+               end if;
+            end loop;
+
+            Table_Level := Table_Level - 1;
+            return To_String (Result);
+
+         exception
+            when Text_IO.End_Error =>
+               Fatal_Error ("found end of file, expected " & End_Table_Token);
+               return "";
+         end Parse_Table;
+
+         function Parse_If (Condition       : in String  := "";
+                            Check_Condition : in Boolean := True)
+           return String
+         is
+            Buffer        : Buffer_Type;
+            Trimed_Buffer : Buffer_Type;
+            Last          : Natural := 0;
+            Lines         : Unbounded_String;
+         begin
+            --  if no need to check condition or condition is true
+
+            if Check_Condition = False
+              or else Exist (Translations, Condition, "TRUE")
+            then
+
+               loop
+
+                  Get_Next_Line (Buffer, Trimed_Buffer, Last);
+
+                  if Last = 0 then
+                     Lines := Lines & ASCII.LF;
+                  else
+                     if Trimed_Buffer (Table_Range) = Table_Token then
+                        Lines := Lines & Parse_Table;
+
+                     elsif Trimed_Buffer (If_Range) = If_Token then
+                        Lines := Lines & Parse_If
+                          (Fixed.Trim
+                           (Trimed_Buffer (If_Range'Last + 2 .. Last),
+                            Both));
+
+                     elsif Trimed_Buffer (Include_Range) = Include_Token then
+                        Lines := Lines & Parse_Include
+                          (Fixed.Trim
+                           (Trimed_Buffer (Include_Range'Last + 2 .. Last),
+                            Both));
+
+                     elsif Trimed_Buffer (End_If_Range) = End_If_Token then
+                        exit;
+
+                     elsif Trimed_Buffer (Else_Range) = Else_Token then
+
+                        --  skip to End_If_Token at the same level
+                        If_Level := 0;
+
+                        loop
+                           Get_Next_Line (Buffer, Trimed_Buffer, Last);
+
+                           if Trimed_Buffer (If_Range) = If_Token then
+                              If_Level := If_Level + 1;
+                           end if;
+
+                           if Trimed_Buffer (End_If_Range) = End_If_Token then
+
+                              if If_Level = 0 then
+                                 exit;
+                              else
+                                 If_Level := If_Level - 1;
+                              end if;
+                           end if;
+                        end loop;
+
+                        exit;
+
+                     elsif
+                       Trimed_Buffer (End_Table_Range) = End_Table_Token
+                     then
+                        Fatal_Error (End_Table_Token & " found, "
+                                     & End_If_Token & " expected.");
+
+                     else
+                        Lines := Lines & Buffer (1 .. Last) & ASCII.LF;
+
+                     end if;
+                  end if;
+               end loop;
+
+               --  We want to translate if's body only if it is not inside a
+               --  table. In this case we return the if's body untranslated.
+               --  The translation will take place in the table using the
+               --  table translation mechanism (i.e. using tag's values set).
+
+               if Table_Level = 0 then
+                  Translate (Lines);
+               end if;
+
+               return To_String (Lines);
+
+            else
+
+               --  condition is false, we skip lines until End_If_Token (at
+               --  the same level) or if we found a Else_Token part we parse
+               --  it.
+
+               If_Level := 0;
+
+               loop
+                  Get_Next_Line (Buffer, Trimed_Buffer, Last);
+
+                  if Trimed_Buffer (If_Range) = If_Token then
+                     If_Level := If_Level + 1;
+                  end if;
+
+                  if If_Level = 0
+                    and then Trimed_Buffer (Else_Range) = Else_Token
+                  then
+                     --  else part found, parse else-block as a main-if block
+                     --  without checking the condition.
+
+                     return Parse_If (Check_Condition => False);
+                  end if;
+
+                  if Trimed_Buffer (End_If_Range) = End_If_Token then
+
+                     if If_Level = 0 then
+                        exit;
+                     else
+                        If_Level := If_Level - 1;
+                     end if;
+                  end if;
+               end loop;
+
+               return "";
+
+            end if;
+         exception
+            when Text_IO.End_Error =>
+               Fatal_Error ("found end of file, expected " & End_If_Token);
+               return "";
+         end Parse_If;
+
+         function Parse_Include (Filename : in String) return String is
+
+            --  returns last directory separator in Filename.
+            --  0 means that there is not directory separator in Filename.
+
+            function Dir_Sep_Index (Filename : in String) return Natural is
+               use Ada.Strings;
+            begin
+               return Fixed.Index (Filename,
+                                   Set   => Maps.To_Set ("/\"),
+                                   Going => Strings.Backward);
+            end Dir_Sep_Index;
+
+            K      : Natural;
+
+         begin
+            --  if the file to be included is specified with a path or if
+            --  there is no path specified with the current template then
+            --  we will open the included template as specified.
+
+            K := Dir_Sep_Index (Template_Filename);
+
+            if Dir_Sep_Index (Filename) /= 0 or else K = 0 then
+               return Parse (Filename, Translations);
+            else
+
+               --  concat current template file pathname with the included
+               --  filename. it means that we want to open the new template
+               --  filename in the directory were the current template is.
+
+               return Parse (Template_Filename (1 .. K) & Filename,
+                             Translations);
+            end if;
+
+         exception
+            when others =>
+               Fatal_Error ("include file " & Filename & " not found.");
+               return "";
+         end Parse_Include;
+
+      begin
+         if Line'Length /= 0 then
+
+            if Trimed_Buffer (Table_Range) = Table_Token then
+
+               return Parse_Table;
+
+            elsif Trimed_Buffer (If_Range) = If_Token then
+
+               return Parse_If
+                 (Fixed.Trim
+                  (Trimed_Buffer (If_Range'Last + 2 .. Last), Both));
+
+            elsif Trimed_Buffer (Include_Range) = Include_Token then
+
+               return Parse_Include
+                 (Fixed.Trim
+                  (Trimed_Buffer (Include_Range'Last + 2 .. Last), Both));
+
+            else
+               Str := To_Unbounded_String (Buffer (1 .. Last));
+               Translate (Str);
+               return To_String (Str) & ASCII.LF;
+            end if;
+
+         else
+            --  empty line
+
+            return String'(1 => ASCII.LF);
+
+         end if;
+      end Parse;
 
    begin
       Text_IO.Open (File => File,
@@ -65,15 +525,11 @@ package body Templates_Parser is
                     Mode => Text_IO.In_File);
 
       while not Text_IO.End_Of_File (File) loop
-         Text_IO.Get_Line (File, Buffer, Last);
 
-         if Last /= 0 then
-            Str := Strings_1024.To_Bounded_String (Buffer (1 .. Last));
-            Translate (Str);
-            Result := Result & Strings_1024.To_String (Str);
-         end if;
+         Get_Next_Line (Buffer, Trimed_Buffer, Last);
 
-         Result := Result & ASCII.LF;
+         Result := Result & Parse (Line => Buffer (1 .. Last));
+
       end loop;
 
       Text_IO.Close (File);
--- src/templates_parser.ads
+++ src/templates_parser.ads
@@ -1,20 +1,98 @@
 
--- $Id$
+--  $Id$
 
---  this package is used to parse an text template and produce a text file.
+--  This package is used to parse an text template and produce a text file.
 --  A template is a text file containing some specials variables. A
 --  variable, in the text template must start with @@_ and end with _@@ by
 --  default.
 --
 --  The parse command below takes a text template file and replace the
---  variables by the corresponding values contain in the Translate_Table.
+--  variables by the corresponding values contained in the Translate_Table.
+--
+--  Special tags can be inserted in the template starting at column
+--  1. Characters following the tag are discarded. This "free" space can be
+--  used to put comments in the template.
+--
+--  These tags are :
+--
+--  @@TABLE@@
+--  ---------
+--
+--  This tag is used to build a table. The lines between @@TABLE@@ and
+--  @@END_TABLE@@ will be parsed for each values specified in the tag.
+--
+--  For example :
+--
+--  @@TABLE@@
+--      @@_col1_@@     @@_col2_@@
+--      ********       ********
+--  @@END_TABLE@@
+--
+--  With col1 = "cell 1.1|cell 1.2" and col2 = "cell 2.1| cell 2.2"
+--  (default separator is a pipe character)
+--
+--  will produce :
+--      cell 1.1       cell 2.1
+--      ********       ********
+--      cell 1.2       cell 2.2
+--      ********       ********
+--
+--  Note that the number of lines produced will be the equal to the smallest
+--  number of values in the tags used in the table.
+--
+--  @@IF@@
+--  ------
+--
+--  This tag is used to parse conditionaly part of the template.
+--
+--  For example :
+--
+--  @@IF@@ @@_T1_@@
+--     ligne 1
+--     ligne 2 avec un tag @@_T6_@@
+--  @@ELSE@@
+--     line 3
+--  @@END_IF@@
+--
+--  The token just after @@IF@@ is a tag name. If this tag is defined
+--  and the value is TRUE then following lines (until @@END_IF@@ or @@ELSE@@)
+--  are parsed otherwise the else part is parsed if it is defined (i.e. the
+--  else part is optional).
+--
+--  @@INCLUDE@@
+--  -----------
+--
+--  This tag is used to include/inline another template.
+--
+--  For example :
+--
+--  @@INCLUDE@@ another_template.tmplt
+--
+--  This command will inline the file another_template.tmplt at the point of
+--  this command.
+--
+--
+--  Note that all commands can be mixed together as in :
+--
+--  @@IF@@ @@_OK_@@
+--     well this is fine.
+--  @@TABLE@@
+--  @@_COL1_@@ @@_COL2_@@
+--  @@END_TABLE@@
+--  @@ELSE@@
+--  @@INCLUDE@@ error_template.tmplt
+--  @@ENDIF@@
+--
 
-with Ada.Strings.Bounded;
+with Ada.Strings.Unbounded;
 
 package Templates_Parser is
 
-   Default_Begin_Tag : constant String := "@@_";
-   Default_End_Tag   : constant String := "_@@";
+   Template_Error : exception;
+
+   Default_Begin_Tag : constant String    := "@@_";
+   Default_End_Tag   : constant String    := "_@@";
+   Default_Separator : constant Character := '|';
 
    type Association is private;
 
@@ -24,31 +102,40 @@ package Templates_Parser is
 
    function Assoc (Variable  : in String;
                    Value     : in String;
-                   Begin_Tag : in String := Default_Begin_Tag;
-                   End_Tag   : in String := Default_End_Tag)
+                   Begin_Tag : in String    := Default_Begin_Tag;
+                   End_Tag   : in String    := Default_End_Tag;
+                   Separator : in Character := Default_Separator)
                    return Association;
    --  build an Association to be added to a Translate_Table.
+
+   function Assoc (Variable  : in String;
+                   Value     : in Boolean;
+                   Begin_Tag : in String    := Default_Begin_Tag;
+                   End_Tag   : in String    := Default_End_Tag)
+                   return Association;
+   --  build an Association to be added to a Translate_Table. It set an assoc
+   --  for variable to "TRUE" if value is true and "FALSE" otherwise.
 
    function Parse (Template_Filename : in String;
                    Translations      : in Translate_Table := No_Translation)
                    return String;
    --  parse the Template_File replacing variables' occurences by the
-   --  corresponding values.
+   --  corresponding values. See template file syntax above.
 
 private
 
-   package Strings_1024 is
-     new Ada.Strings.Bounded.Generic_Bounded_Length (1_024);
-   subtype B_String is Strings_1024.Bounded_String;
+   use Ada.Strings.Unbounded;
 
    type Association is
       record
-         Variable : B_String;
-         Value    : B_String;
+         Variable  : Unbounded_String;
+         Value     : Unbounded_String;
+         Separator : Character;
       end record;
 
    No_Translation : constant Translate_Table
-     := (2 .. 1 => Association'(Strings_1024.To_Bounded_String (""),
-                                Strings_1024.To_Bounded_String ("")));
+     := (2 .. 1 => Association'(Null_Unbounded_String,
+                                Null_Unbounded_String,
+                                ASCII.Nul));
 
 end Templates_Parser;

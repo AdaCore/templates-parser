f8f1a4b37095f47632d7a6733a4267ea39b8e79f
Pascal Obry <obry@adacore.com>
2002-06-22 09:56:26 +0000

    Initial revision.

---
+++ src/templates_parser-input.adb
@@ -0,0 +1,110 @@
+------------------------------------------------------------------------------
+--                             Templates Parser                             --
+--                                                                          --
+--                            Copyright (C) 2002                            --
+--                               Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  This is the standard version to be used with the standalone version of
+--  Templates_Parser.
+
+with Ada.Text_IO;
+with Ada.Unchecked_Deallocation;
+
+package body Templates_Parser.Input is
+
+   use Ada;
+
+   type File_Record is record
+      File : Text_IO.File_Type;
+   end record;
+
+   procedure Free is new Ada.Unchecked_Deallocation (File_Record, File_Type);
+
+   -----------
+   -- Close --
+   -----------
+
+   procedure Close (File : in out File_Type) is
+   begin
+      if File = null then
+         raise Text_IO.Status_Error;
+      else
+         Text_IO.Close (File.File);
+      end if;
+      Free (File);
+   exception
+      when others =>
+         Free (File);
+   end Close;
+
+   -----------------
+   -- End_Of_File --
+   -----------------
+
+   function End_Of_File (File : in File_Type) return Boolean is
+   begin
+      if File = null then
+         raise Text_IO.Status_Error;
+      else
+         return Text_IO.End_Of_File (File.File);
+      end if;
+   end End_Of_File;
+
+   --------------
+   -- Get_Line --
+   --------------
+
+   procedure Get_Line
+     (File   : in     File_Type;
+      Buffer :    out String;
+      Last   :    out Natural) is
+   begin
+      if File = null then
+         raise Text_IO.Status_Error;
+      else
+         Text_IO.Get_Line (File.File, Buffer, Last);
+      end if;
+   end Get_Line;
+
+   ----------
+   -- Open --
+   ----------
+
+   procedure Open
+     (File : in out File_Type;
+      Name : in     String;
+      Form : in     String    := "") is
+   begin
+      if File /= null then
+         Close (File);
+      end if;
+
+      File := new File_Record;
+      Text_IO.Open (File.File, Text_IO.In_File, Name, Form);
+   end Open;
+
+end Templates_Parser.Input;

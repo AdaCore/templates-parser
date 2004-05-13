1553e36a311336eb277b07ac1f59cf17a5a9ca41
Pascal Obry <obry@adacore.com>
2004-05-13 21:12:14 +0000

    (Vector): Renamed Composite. (Nested_Level): New routine. Update copyright notice.

--- src/templates_parser-query.adb
+++ src/templates_parser-query.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                             Templates Parser                             --
 --                                                                          --
---                          Copyright (C) 2003-2004                         --
+--                         Copyright (C) 2003-2004                          --
 --                               Pascal Obry                                --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
@@ -34,6 +34,23 @@ package body Templates_Parser.Query is
 
    use Ada;
 
+   ---------------
+   -- Composite --
+   ---------------
+
+   function Composite
+     (Association : in Templates_Parser.Association)
+      return Tag is
+   begin
+      if Association.Kind = Composite then
+         return Association.Comp_Value;
+      else
+         Exceptions.Raise_Exception
+           (Constraint_Error'Identity,
+            Variable (Association) & " is not a composite tag.");
+      end if;
+   end Composite;
+
    ----------
    -- Kind --
    ----------
@@ -45,6 +62,15 @@ package body Templates_Parser.Query is
       return Association.Kind;
    end Kind;
 
+   ------------------
+   -- Nested_Level --
+   ------------------
+
+   function Nested_Level (T : in Tag) return Positive is
+   begin
+      return T.Nested_Level;
+   end Nested_Level;
+
    --------------
    -- Variable --
    --------------
@@ -55,24 +81,5 @@ package body Templates_Parser.Query is
    begin
       return To_String (Association.Variable);
    end Variable;
-
-   ------------
-   -- Vector --
-   ------------
-
-   function Vector
-     (Association : in Templates_Parser.Association)
-      return Vector_Tag is
-   begin
-      if Association.Kind = Composite
-        and then Association.Comp_Value.Nested_Level = 1
-      then
-         return Vector_Tag (Association.Comp_Value);
-      else
-         Exceptions.Raise_Exception
-           (Constraint_Error'Identity,
-            Variable (Association) & " is not a vector tag.");
-      end if;
-   end Vector;
 
 end Templates_Parser.Query;

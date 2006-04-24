9a0f119aad497ab21e9edc322a53024d9ac453ce
Pascal Obry <obry@adacore.com>
2006-04-24 19:59:25 +0000

    Minor reformatting.

--- src/templates_parser.ads
+++ src/templates_parser.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                             Templates Parser                             --
 --                                                                          --
---                        Copyright (C) 1999 - 2006                         --
+--                         Copyright (C) 1999-2006                          --
 --                                 AdaCore                                  --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --

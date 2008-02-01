85a8442b74df864a891a58e504131e4a0ec35d92
Pascal Obry <obry@adacore.com>
2008-02-01 14:32:01 +0000

    Fix copyright style for new tool templatespp

--- tools/templatespp.adb
+++ tools/templatespp.adb
@@ -1,7 +1,8 @@
 ------------------------------------------------------------------------------
 --                             Templates Parser                             --
 --                                                                          --
---                       Copyright (C) 2008, AdaCore                        --
+--                            Copyright (C) 2008                            --
+--                                 AdaCore                                  --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --

9b5d7cc51eb319e810bcb85cccb50f221937c71b
Pascal Obry <obry@adacore.com>
2014-05-10 09:29:56 +0200

    Minor reformatting.

--- docs/confapis.py
+++ docs/confapis.py
@@ -1,29 +1,30 @@
 
-import os, os.path
+import os
+import os.path
 
-API=['src/templates_parser.ads',
-     'src/templates_parser-debug.ads',
-     'src/templates_parser-utils.ads',
-     'xsrc/templates_parser-xml.ads']
+API = ['src/templates_parser.ads',
+       'src/templates_parser-debug.ads',
+       'src/templates_parser-utils.ads',
+       'xsrc/templates_parser-xml.ads']
 
-if os.path.exists("build/apirefs") == False:
-    os.makedirs ("build/apirefs")
+if os.path.exists("build/apirefs") is False:
+    os.makedirs("build/apirefs")
 
 for path in API:
     try:
-        fin = open ("../" + path)
-        fout = open ("build/apirefs/" + os.path.basename(path),'w')
+        fin = open("../" + path)
+        fout = open("build/apirefs/" + os.path.basename(path), 'w')
         outside_private_part = True
         for line in fin:
-            if line.startswith ("end "):
+            if line.startswith("end "):
                 outside_private_part = True
             if outside_private_part:
-                fout.write (line)
+                fout.write(line)
             if line == "private\n":
                 outside_private_part = False
-                fout.write ("   -- implementation removed\n")
+                fout.write("   -- implementation removed\n")
     finally:
         if fin:
-            fin.close ()
+            fin.close()
         if fout:
-            fout.close ()
+            fout.close()
